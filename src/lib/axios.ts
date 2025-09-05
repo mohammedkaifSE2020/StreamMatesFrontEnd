1a30925fd61914929a16973dfd9ad17660a68a39
Mohammed Kaif <kaifmohammed008@gmail.com>
2025-09-05 19:51:46 +0530

    phase correction

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-const BaseURL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api"
-
+const BaseURL = import.meta.env.VITE_MODE === "development" ? "http://localhost:3000/api" : "https://streammatesbackend.onrender.com/api"
+console.log(BaseURL)
 export const axiosInstance = axios.create({
     baseURL : BaseURL,
     withCredentials: true,
